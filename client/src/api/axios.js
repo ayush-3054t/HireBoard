35a1759f020819c0cb6c0d9262027c4bcd2fb068
Ayush Tiwari <ayushtiwari3054@gmail.com>
2026-05-09 19:33:17 +0530

    Fix CORS

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,19 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace('/api', '') : 
-// 'http://localhost:5000'
-"https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app"
-;
+export const API_BASE_URL =
+  "https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 
-    'https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app/api',
-    
-  withCredentials: true
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app/api",
+
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
