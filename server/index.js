ef899079981a29e5d7adf054fdca7b34eaf0777c
Ayush Tiwari <ayushtiwari3054@gmail.com>
2026-05-09 19:56:01 +0530

    Simplify CORS

--- server/index.js
+++ server/index.js
@@ -5,39 +5,26 @@ const morgan = require('morgan');
 const path = require('path');
 require('dotenv').config();
 
-const connectDB = require('./config/db');
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const recruiterRoutes = require('./routes/recruiterRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const jobRoutes = require('./routes/jobRoutes');
-const applicationRoutes = require('./routes/applicationRoutes');
-const companyRoutes = require('./routes/companyRoutes');
-
 const app = express();
 
-const app = express();
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
-// app.use(cors({
-//   origin: true,
-//   credentials: true
-// }));
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  return res.sendStatus(200);
+});
 
-// app.options('*', (req, res) => {
-//   res.header('Access-Control-Allow-Origin', req.headers.origin);
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   return res.sendStatus(200);
-// });
-
-// app.use(helmet({
-//   crossOriginResourcePolicy: {
-//     policy: 'cross-origin'
-//   }
-// }));
+app.use(helmet({
+  crossOriginResourcePolicy: {
+    policy: 'cross-origin'
+  }
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
