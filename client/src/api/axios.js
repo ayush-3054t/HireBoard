b290de080bddd19bec7d7265a4f5dd555c6e410b
Ayush Tiwari <ayushtiwari3054@gmail.com>
2026-05-09 19:01:26 +0530

    Deployment edits

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace('/api', '') : 'http://localhost:5000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace('/api', '') : 
+// 'http://localhost:5000'
+"https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app/"
+;
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
