9dba96c1f12c4b89756e586e4da496a2ae87978a
Ayush Tiwari <ayushtiwari3054@gmail.com>
2026-05-09 19:16:08 +0530

    Fix CORS

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace('/api', '') : 
-// 'http://localhost:5000'
-"https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app"
-;
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app/api' 
+  baseURL: import.meta.env.VITE_API_URL || 
+    'https://hire-board-git-main-ayushs-projects-6fd09652.vercel.app/api',
+    
+  withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
